319da25146329009aa29874a25983652349ef0c9
mayeille <mayeille.courtis@gmail.com>
2019-04-04 17:46:44 +0200

    version définitive sans arduino

--- luminator/app.py
+++ luminator/app.py
@@ -1,25 +1,43 @@
 # coding: utf-8
 from flask import Flask, request, render_template
+import pylibmodbus
+
 app = Flask(__name__)
 
-led_on = True
+ADDRESS_LED = 0
 
 def set_led(request):
-	global led_on
 	if "light" not in request.form:
 		return "light argument is missing", 400
 
 	if request.form["light"] == "on":
-		led_on = True
+		arduino_write_led(True)
 	else:
-		led_on = False	
+		arduino_write_led(False)	
 
 @app.route('/', methods=['GET', 'POST'])    
 def led():
 	if request.method == 'POST':
 		set_led(request)
 	
+	led_on = arduino_read_led()
 	return render_template('pageled.html', led_on=led_on)
-	
-		
 
+def	arduino_write_led(value):
+	global mb
+	if value:
+		v = 1
+	else:
+		v = 0
+
+	mb.write_register(ADDRESS_LED, v)
+
+def arduino_read_led():
+	global mb
+	read_data = mb.read_registers(ADDRESS_LED, 1)
+	return bool(read_data[0])
+
+
+mb = pylibmodbus.ModbusTcp("127.0.0.1", 1502)
+mb.connect()
+# mb.close()
